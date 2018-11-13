5921515b5beca71610a05d744e88eb56d5587623
Rick Richardson <rick@agilestacks.com>
2018-11-13 09:35:16 -0800

    added docker build bits

---
+++ docker/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+REPO=eigenrick
+IMAGE=object-spam
+TAG=0.1
+
+SOURCEDIR=$(dirname ${BASH_SOURCE[0]})
+cd ${SOURCEDIR} && cd ..
+
+docker build -t ${REPO}/${IMAGE}:${TAG} -f docker/Dockerfile .
